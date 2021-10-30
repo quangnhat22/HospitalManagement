2284d81ddc8705c4a0b32480d93afc2c440be423
Phu Quang <20521800@gm.uit.edu.vn>
2021-10-30 16:46:46 +0700

    Add signup form

---
+++ HospitalManagement/View/SignUpWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HospitalManagement.View
+{
+    /// <summary>
+    /// Interaction logic for SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+        public SignUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
