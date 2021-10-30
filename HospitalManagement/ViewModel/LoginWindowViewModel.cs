279c19980c453fa1451a5f17b39fed76a013684b
quangSotech <20520719@gm.uit.edu.vn>
2021-10-30 10:35:53 +0700

    fix login fuction

--- HospitalManagement/ViewModel/LoginWindowViewModel.cs
+++ HospitalManagement/ViewModel/LoginWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalManagement.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,15 +15,7 @@ namespace HospitalManagement.ViewModel
         public ICommand OpenMainWindow { get; set; }
         public LoginWindowViewModel()
         {
-            OpenMainWindow = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) => { ShowMainWindow(p); });
-        }
-        void ShowMainWindow(Window p)
-        {
-            var mainWindow = new MainWindow();
-            Application.Current.MainWindow = mainWindow;
-            mainWindow.Show();
-            //I assume loginWindow is a reference to the LoginWindow being shown
-            p.Close();
+            OpenMainWindow = new LoginWindowCommand();
         }
 
     }
