6ca4544b8bbb03e9a8d6e377a53f0fe8b04d8c88
nghia0111 <20521659@gm.uit.edu.vn>
2021-10-30 22:56:31 +0700

    Add patient class and data
    Add patient class and data

---
+++ HospitalManagement/Model/Patient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace HospitalManagement.Model
+{
+    public class Patient : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        private bool isChecked;
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set { isChecked = value; OnPropertyChanged("IsChecked"); }
+        }
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public SexType Sex { get; set; }
+        public string Birthday { get; set; }
+        public string DateCheckIn { get; set; }
+        public string DoctorName { get; set; }
+        public string Status { get; set; }
+        public string Room { get; set; }
+    }
+}
