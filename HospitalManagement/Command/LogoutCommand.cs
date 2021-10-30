0ea3c705d36ce0f76c915b2f2407f78dfbbdfc49
LocTranDinh <20520236@gm.uit.edu.vn>
2021-10-30 20:01:48 +0700

    Add logout Function
    Add logout Function
    - Logout Command
    - Icommand in MainViewModel

---
+++ HospitalManagement/Command/LogoutCommand.cs
@@ -0,0 +1,30 @@
+﻿using HospitalManagement.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HospitalManagement.Command
+{
+    class LogoutCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return parameter == null ? false : true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Window mainWindow = parameter as Window;
+            LoginWindow loginWindow = new LoginWindow();
+            Application.Current.MainWindow = loginWindow;
+            loginWindow.Show();
+            mainWindow.Close();
+        }
+    }
+}
