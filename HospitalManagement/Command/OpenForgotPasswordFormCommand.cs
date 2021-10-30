0b9241c136f2bf7fd469c65a520f155db98b3dca
nghia0111 <20521659@gm.uit.edu.vn>
2021-10-31 00:07:36 +0700

    add forgot password window
    add forgot password window

---
+++ HospitalManagement/Command/OpenForgotPasswordFormCommand.cs
@@ -0,0 +1,30 @@
+﻿using HospitalManagement.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HospitalManagement.Command
+{
+    internal class OpenForgotPasswordFormCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return parameter == null ? false : true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Window window = parameter as Window;
+            var mainWindow = new ForgotPasswordWindow();
+            Application.Current.MainWindow = mainWindow;
+            mainWindow.Show();
+            window.Close();
+        }
+    }
+}
--- HospitalManagement/ViewModel/LoginWindowViewModel.cs
+++ HospitalManagement/ViewModel/LoginWindowViewModel.cs
@@ -14,10 +14,12 @@ namespace HospitalManagement.ViewModel
      {
         public ICommand OpenMainWindow { get; set; }
         public ICommand OpenSignUpForm { get; set; }
+        public ICommand OpenForgotPasswordForm { get; set; }
         public LoginWindowViewModel()
         {
             OpenMainWindow = new LoginWindowCommand();
-            OpenSignUpForm = new OpenSignUpFormCommand();  
+            OpenSignUpForm = new OpenSignUpFormCommand();
+            OpenForgotPasswordForm = new OpenForgotPasswordFormCommand();
         }
 
     }
