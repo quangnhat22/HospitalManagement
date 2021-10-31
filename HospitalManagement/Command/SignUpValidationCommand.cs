7f39b2131d3c0c8d3b0e650f47086807e9091775
Phu Quang <92578042+phuquang14722@users.noreply.github.com>
2021-11-01 00:07:03 +0700

    Change sign up validate algorithm

--- HospitalManagement/Command/SignUpValidationCommand.cs
+++ HospitalManagement/Command/SignUpValidationCommand.cs
@@ -12,147 +12,83 @@ using System.Windows;
 
 namespace HospitalManagement.Command
 {
-    class SignUpValidationCommand : ICommand, INotifyDataErrorInfo
+    class SignUpValidationCommand : ICommand
     {
-        private string _firstName;
-        public string FirstName { get { return _firstName; } set { _firstName = value; OnPropertyChanged("FirstName"); } }
-        private string _lastName;
-        public string LastName { get { return _lastName; } set { _lastName = value; OnPropertyChanged("LastName"); } }
-        private string _userName;
-        public string UserName { get { return _userName; } set { _userName = value; OnPropertyChanged("UserName"); } }
-        private string _email;
-        public string Email { get { return _email; } set { _email = value; OnPropertyChanged("Email"); } }
-        private string _password;
-        public string Password { get { return _password; } set { _password = value; OnPropertyChanged("Password"); } }
-        private string _confirmPassword;
-        public string ConfirmPassword { get { return _confirmPassword; } set { _confirmPassword = value; OnPropertyChanged("ConfirmPassword"); } }
-        private string _birthdate;
-        public string Birthdate { get { return _birthdate; } set { _birthdate = value; OnPropertyChanged("Birthdate"); } }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string name)
-        {
-            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(name));
-            Validate(name);
-        }
-
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter)
         {
-            return HasErrors ? false : true;
+            return parameter == null ? false : true;
         }
 
         public void Execute(object parameter)
         {
-            Validate(nameof(FirstName));
-            Validate(nameof(LastName));
-            Validate(nameof(UserName));
-            Validate(nameof(Email));
-            Validate(nameof(Password));
-            Validate(nameof(ConfirmPassword));
-            Validate(nameof(Birthdate));
-            if (!HasErrors) MessageBox.Show("Gọi command thành công");
-        }
-
-        private void Validate(string changedPropertyName)
-        {
-            switch (changedPropertyName)
+            if (Check(parameter as SignUpWindow))
             {
-                case nameof(FirstName):
-                    if (string.IsNullOrWhiteSpace(FirstName))
-                    {
-                        _ValidationErrorsByProperty[nameof(FirstName)] = new List<object> { "Vui lòng nhập họ" };
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(FirstName)));
-                    }
-                    else if (_ValidationErrorsByProperty.Remove(nameof(FirstName)))
-                    {
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(FirstName)));
-                    }
-                    break;
-                case nameof(LastName):
-                    if (string.IsNullOrWhiteSpace(LastName))
-                    {
-                        _ValidationErrorsByProperty[nameof(LastName)] = new List<object> { "Vui lòng nhập tên" };
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(LastName)));
-                    }
-                    else if (_ValidationErrorsByProperty.Remove(nameof(LastName)))
-                    {
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(LastName)));
-                    }
-                    break;
-                case nameof(UserName):
-                    if (string.IsNullOrWhiteSpace(UserName))
-                    {
-                        _ValidationErrorsByProperty[nameof(UserName)] = new List<object> { "Vui lòng nhập tên đăng nhập" };
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(UserName)));
-                    }
-                    else if (_ValidationErrorsByProperty.Remove(nameof(UserName)))
-                    {
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(UserName)));
-                    }
-                    break;
-                case nameof(Email):
-                    if (string.IsNullOrWhiteSpace(Email))
-                    {
-                        _ValidationErrorsByProperty[nameof(Email)] = new List<object> { "Vui lòng nhập email" };
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Email)));
-                    }
-                    else if (_ValidationErrorsByProperty.Remove(nameof(Email)))
-                    {
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Email)));
-                    }
-                    break;
-                case nameof(Password):
-                    if (string.IsNullOrWhiteSpace(Password))
-                    {
-                        _ValidationErrorsByProperty[nameof(Password)] = new List<object> { "Vui lòng nhập mật khẩu" };
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Password)));
-                    }
-                    else if (_ValidationErrorsByProperty.Remove(nameof(Password)))
-                    {
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Password)));
-                    }
-                    break;
-                case nameof(ConfirmPassword):
-                    if (string.IsNullOrWhiteSpace(ConfirmPassword))
-                    {
-                        _ValidationErrorsByProperty[nameof(ConfirmPassword)] = new List<object> { "Vui lòng nhập lại mật khẩu" };
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(ConfirmPassword)));
-                    }
-                    else if (_ValidationErrorsByProperty.Remove(nameof(ConfirmPassword)))
-                    {
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(ConfirmPassword)));
-                    }
-                    break;
-                case nameof(Birthdate):
-                    if (string.IsNullOrWhiteSpace(Birthdate))
-                    {
-                        _ValidationErrorsByProperty[nameof(Birthdate)] = new List<object> { "Vui lòng nhập ngày sinh" };
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Birthdate)));
-                    }
-                    else if (_ValidationErrorsByProperty.Remove(nameof(Birthdate)))
-                    {
-                        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Birthdate)));
-                    }
-                    break;
+                MessageBox.Show("Đăng ký thành công!");
             }
         }
 
-        public IEnumerable GetErrors(string propertyName)
+        public bool Check(SignUpWindow mw)
         {
-            if (_ValidationErrorsByProperty.TryGetValue(propertyName, out List<object> errors))
+            if (mw == null) return false;
+            if (string.IsNullOrWhiteSpace(mw.txbHo.Text))
             {
-                return errors;
+                MessageBox.Show("Vui lòng nhập họ");
+                mw.txbHo.Focus();
+                return false;
             }
-            return Array.Empty<object>();
+
+            if (string.IsNullOrWhiteSpace(mw.txbTen.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tên");
+                mw.txbTen.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(mw.txbTenDangNhap.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tên đăng nhập");
+                mw.txbTenDangNhap.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(mw.txbEmail.Text))
+            {
+                MessageBox.Show("Vui lòng nhập email");
+                mw.txbEmail.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(mw.txbMatKhau.Password))
+            {
+                MessageBox.Show("Vui lòng nhập mật khẩu");
+                mw.txbMatKhau.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(mw.txbNhapLaiMatKhau.Password))
+            {
+                MessageBox.Show("Vui lòng nhập lại mật khẩu");
+                mw.txbNhapLaiMatKhau.Focus();
+                return false;
+            }
+
+            if (mw.txbMatKhau.Password != mw.txbNhapLaiMatKhau.Password)
+            {
+                MessageBox.Show("Mật khẩu đã nhập không khớp");
+                mw.txbNhapLaiMatKhau.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(mw.txbNgaySinh.Text))
+            {
+                MessageBox.Show("Vui lòng nhập ngày sinh");
+                mw.txbNgaySinh.Focus();
+                return false;
+            }
+
+            return true;
         }
-
-        private readonly Dictionary<string, List<object>> _ValidationErrorsByProperty = new Dictionary<string, List<object>>();
-
-        public bool HasErrors => _ValidationErrorsByProperty.Any();
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
     }
 }
