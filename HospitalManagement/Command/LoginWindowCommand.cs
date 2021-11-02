84502989090de8f3e4741bb37b72d0d10cd8810f
quangSotech <20520719@gm.uit.edu.vn>
2021-11-02 15:53:14 +0700

    fix thread login form

--- HospitalManagement/Command/LoginWindowCommand.cs
+++ HospitalManagement/Command/LoginWindowCommand.cs
@@ -5,9 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace HospitalManagement.Command
 {
@@ -54,8 +56,17 @@ namespace HospitalManagement.Command
         {
             Window window = Application.Current.MainWindow as Window;
             Application.Current.MainWindow = new MainWindow();
-            Application.Current.MainWindow.Show();
             window.Close();
+            Thread windowThread = new Thread(new ThreadStart(() =>
+            {
+                window.Closed += (s, e) =>
+                Dispatcher.CurrentDispatcher.BeginInvokeShutdown(DispatcherPriority.Background);
+                System.Windows.Threading.Dispatcher.Run();
+            }));
+            windowThread.SetApartmentState(ApartmentState.STA);
+            windowThread.IsBackground = true;
+            windowThread.Start();
+            Application.Current.MainWindow.Show();
         }
 
         private bool CheckPassword(string plainText)
--- HospitalManagement/Command/OpenSignUpFormCommand.cs
+++ HospitalManagement/Command/OpenSignUpFormCommand.cs
@@ -3,9 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace HospitalManagement.Command
 {
@@ -27,8 +29,17 @@ namespace HospitalManagement.Command
             Window window = parameter as Window;
             var signUpWindow = new SignUpWindow();
             Application.Current.MainWindow = signUpWindow;
-            signUpWindow.Show();
             window.Close();
+            Thread windowThread = new Thread(new ThreadStart(() =>
+            {
+                window.Closed += (s, e) =>
+                Dispatcher.CurrentDispatcher.BeginInvokeShutdown(DispatcherPriority.Background);
+                System.Windows.Threading.Dispatcher.Run();
+            }));
+            windowThread.SetApartmentState(ApartmentState.STA);
+            windowThread.IsBackground = true;
+            windowThread.Start();
+            signUpWindow.Show(); 
         }
     }
 }
