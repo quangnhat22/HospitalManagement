526e76ae452213b766fcd317641086d448ec940a
LocTranDinh <20520236@gm.uit.edu.vn>
2021-11-02 13:22:44 +0700

    Move hash function to another class

--- HospitalManagement/Command/LoginWindowCommand.cs
+++ HospitalManagement/Command/LoginWindowCommand.cs
@@ -1,9 +1,9 @@
-﻿using HospitalManagement.View;
+﻿using HospitalManagement.Utils;
+using HospitalManagement.View;
 using HospitalManagement.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -60,7 +60,7 @@ namespace HospitalManagement.Command
 
         private bool CheckPassword(string plainText)
         {
-            string hashedPasswordInput = Hash(plainText);
+            string hashedPasswordInput = Encryptor.Hash(plainText);
             string databasePassword = "cdd96d3cc73d1dbdaffa03cc6cd7339b";
             return string.Compare(databasePassword, hashedPasswordInput, true) == 0;
         }
@@ -69,35 +69,6 @@ namespace HospitalManagement.Command
         {
             string databaseUsername = "admin";
             return username == databaseUsername;
-        }
-
-        private string Hash(string plainText)
-        {
-            return CreateMD5(Base64Encode(plainText));
-        }
-
-        private static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-            return Convert.ToBase64String(plainTextBytes);
-        }
-
-        private static string CreateMD5(string input)
-        {
-            // Use input string to calculate MD5 hash
-            using (MD5 md5 = MD5.Create())
-            {
-                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
-
-                // Convert the byte array to hexadecimal string
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < hashBytes.Length; i++)
-                {
-                    sb.Append(hashBytes[i].ToString("X2"));
-                }
-                return sb.ToString();
-            }
         }
     }
 }
---
+++ HospitalManagement/Utils/Encryptor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace HospitalManagement.Utils
+{
+    class Encryptor
+    {
+        public static string Hash(string plainText)
+        {
+            return CreateMD5(Base64Encode(plainText));
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        private static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
