9fdd14ddd2a170e656c167aec3968eb4113287f0
Phu Quang <92578042+phuquang14722@users.noreply.github.com>
2021-11-09 23:09:13 +0700

    Add NurseInformationForm

---
+++ HospitalManagement/ViewModel/NurseInformationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalManagement.Model;
+
+namespace HospitalManagement.ViewModel
+{
+    class NurseInformationViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        private Nurse nurse;
+
+        public Nurse Nurse
+        {
+            get { return nurse; }
+            set { nurse = value; OnPropertyChanged("Nurse"); }
+        }
+
+        public NurseInformationViewModel(Nurse nurse)
+        {
+            Nurse = nurse;
+        }
+    }
+}
