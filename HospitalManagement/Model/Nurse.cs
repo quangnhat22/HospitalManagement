38b3216354ca2c7dd868fc387005cf9055dca17b
Phu Quang <92578042+phuquang14722@users.noreply.github.com>
2021-11-09 20:25:11 +0700

    Change properties of nurse model

--- HospitalManagement/Model/Nurse.cs
+++ HospitalManagement/Model/Nurse.cs
@@ -25,19 +25,15 @@ namespace HospitalManagement.Model
         }
 
         public int ID { get; set; }
-
-        public string Name { get; set; }
-
-        public int Age { get; set; }
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Phone { get; set; }
-
         public string Mail { get; set; }
-
+        public string Address { get; set; }
         public SexType Sex { get; set; }
-
         public string Birthday { get; set; }
-
+        public string Specialization { get; set; }
         public string Rule { get; set; }
+        public string Note { get; set; }
     }
 }
