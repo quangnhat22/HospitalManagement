7c0c9d9d9ed2e5c6f669c97b9d7643edc1288c56
LocTranDinh <20520236@gm.uit.edu.vn>
2021-11-12 10:16:50 +0700

    Update more security

--- HospitalManagement/Utils/Encryptor.cs
+++ HospitalManagement/Utils/Encryptor.cs
@@ -11,13 +11,30 @@ namespace HospitalManagement.Utils
     {
         public static string Hash(string plainText)
         {
-            return CreateMD5(Base64Encode(plainText));
+            return CreateMD5(CreateSHA1WithSalt(Base64Encode(plainText)));
         }
 
         private static string Base64Encode(string plainText)
         {
             var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
             return Convert.ToBase64String(plainTextBytes);
+        }
+
+        private static string CreateSHA1WithSalt(string input)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input + "FHMS");
+                byte[] hashBytes = sha1.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
         }
 
         private static string CreateMD5(string input)
