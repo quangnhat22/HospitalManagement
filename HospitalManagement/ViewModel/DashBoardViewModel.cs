fa2d52394ef3f7ccb0f640af890a3bf5cdaf7717
quangSotech <20520719@gm.uit.edu.vn>
2021-11-14 00:17:55 +0700

    update dashboard

--- HospitalManagement/ViewModel/DashBoardViewModel.cs
+++ HospitalManagement/ViewModel/DashBoardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalManagement.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace HospitalManagement.ViewModel
 {
     class DashBoardViewModel : BaseViewModel
     {
+        private int staffCount;
+        private int patientCount;
+        private int bedCount;
+
+        public int StaffCount { get => staffCount; set => staffCount = value; }
+        public int PatientCount { get => patientCount; set => patientCount = value; }
+        public int BedCount { get => bedCount; set => bedCount = value; }
+
+        public DashBoardViewModel()
+        {
+            StaffCount = DataProvider.Ins.DB.BACSIs.Count() + DataProvider.Ins.DB.YTAs.Count();
+            PatientCount = DataProvider.Ins.DB.BENHNHANs.Count();
+            BedCount = DataProvider.Ins.DB.PHONGs.Count() * 6;
+        }
+
+        
     }
 }
