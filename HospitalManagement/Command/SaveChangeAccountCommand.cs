7cdd301b3f1710120983c71afcf00c255219ad90
Bùi Minh Tuấn <92067348+tuan20520342@users.noreply.github.com>
2021-11-15 19:42:32 +0700

    Fix Account Form

--- HospitalManagement/Command/SaveChangeAccountCommand.cs
+++ HospitalManagement/Command/SaveChangeAccountCommand.cs
@@ -35,54 +35,54 @@ namespace HospitalManagement.Command
         public bool Check(ChangeAccountWindow cw)
         {
             if (cw == null) return false;
-            if (string.IsNullOrWhiteSpace(cw.txbNewPassword.Text) && string.IsNullOrWhiteSpace(cw.txbNewRepassword.Text))
-            {
-                if(string.IsNullOrWhiteSpace(cw.txbLastName.Text))
-                {
-                    NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập Họ");
-                    notifyWindow.ShowDialog();
-                    cw.txbLastName.Focus();
-                    return false;
-                }
-                if (string.IsNullOrWhiteSpace(cw.txbFistName.Text))
-                {
-                    NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập Tên");
-                    notifyWindow.ShowDialog();
-                    cw.txbFistName.Focus();
-                    return false;
-                }
+            //if (string.IsNullOrWhiteSpace(cw.txbNewPassword.Text) && string.IsNullOrWhiteSpace(cw.txbNewRepassword.Text))
+            //{
+            //    if(string.IsNullOrWhiteSpace(cw.txbLastName.Text))
+            //    {
+            //        NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập Họ");
+            //        notifyWindow.ShowDialog();
+            //        cw.txbLastName.Focus();
+            //        return false;
+            //    }
+            //    if (string.IsNullOrWhiteSpace(cw.txbFistName.Text))
+            //    {
+            //        NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập Tên");
+            //        notifyWindow.ShowDialog();
+            //        cw.txbFistName.Focus();
+            //        return false;
+            //    }
 
-                if (string.IsNullOrWhiteSpace(cw.tbDateTimePicker.Text))
-                {
-                    NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập ngày sinh");
-                    notifyWindow.ShowDialog();
-                    cw.txbFistName.Focus();
-                    return false;
-                }
-                if (string.IsNullOrWhiteSpace(cw.tbDateTimePicker.Text))
-                {
-                    NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập ngày sinh");
-                    notifyWindow.ShowDialog();
-                    cw.txbFistName.Focus();
-                    return false;
-                }
+            //    if (string.IsNullOrWhiteSpace(cw.tbDateTimePicker.Text))
+            //    {
+            //        NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập ngày sinh");
+            //        notifyWindow.ShowDialog();
+            //        cw.txbFistName.Focus();
+            //        return false;
+            //    }
+            //    if (string.IsNullOrWhiteSpace(cw.tbDateTimePicker.Text))
+            //    {
+            //        NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập ngày sinh");
+            //        notifyWindow.ShowDialog();
+            //        cw.txbFistName.Focus();
+            //        return false;
+            //    }
 
-                return false;
-                //if (string.IsNullOrWhiteSpace(cw.cbSex.Text))
-                //{
-                //    NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập họ tên");
-                //    notifyWindow.ShowDialog();
-                //    cw.txbNameUser.Focus();
-                //    return false;
-                //}
-                //if (string.IsNullOrWhiteSpace(cw.txbEmail.Text))
-                //{
-                //    NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập địa chỉ Email");
-                //    notifyWindow.ShowDialog();
-                //    cw.txbNameUser.Focus();
-                //    return false;
-                //}
-            }
+            //    return false;
+            //    //if (string.IsNullOrWhiteSpace(cw.cbSex.Text))
+            //    //{
+            //    //    NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập họ tên");
+            //    //    notifyWindow.ShowDialog();
+            //    //    cw.txbNameUser.Focus();
+            //    //    return false;
+            //    //}
+            //    //if (string.IsNullOrWhiteSpace(cw.txbEmail.Text))
+            //    //{
+            //    //    NotifyWindow notifyWindow = new NotifyWindow("Warning", "Vui lòng nhập địa chỉ Email");
+            //    //    notifyWindow.ShowDialog();
+            //    //    cw.txbNameUser.Focus();
+            //    //    return false;
+            //    //}
+            //}
             //var users = forgotPasswordFormViewModel?.db?.DB?.USERs.Where(x => x.USERNAME == mw.tbUsername.Text && x.EMAIL == mw.tbMailAddress.Text);
             //if (users == null || users.Count() == 0)
             //{
