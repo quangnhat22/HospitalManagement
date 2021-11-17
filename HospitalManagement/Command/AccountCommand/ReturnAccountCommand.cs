cf3d45b09ee0d3b280686809081a3088cc17eef3
quangSotech <20520719@gm.uit.edu.vn>
2021-11-17 23:21:35 +0700

    update btn account

---
+++ HospitalManagement/Command/AccountCommand/ReturnAccountCommand.cs
@@ -0,0 +1,34 @@
+﻿using HospitalManagement.View.Others;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HospitalManagement.Command.AccountCommand
+{
+    internal class ReturnAccountCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ChangeAccountWindow window = parameter as ChangeAccountWindow;
+            ChangeAccountWindow account = new ChangeAccountWindow();
+            Application.Current.MainWindow = account;
+            window.Close();
+            account.ShowDialog(); ;
+        }
+    }
+}
