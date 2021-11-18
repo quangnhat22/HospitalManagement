56439eeba543a2b3ee650c4a4b24e6179c98ceef
quangSotech <20520719@gm.uit.edu.vn>
2021-11-18 11:10:08 +0700

    update piechart database

--- HospitalManagement/Command/DashBoardCommand/InitPieChartCommand.cs
+++ HospitalManagement/Command/DashBoardCommand/InitPieChartCommand.cs
@@ -5,6 +5,7 @@ using LiveCharts;
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -42,17 +43,25 @@ namespace HospitalManagement.Command.DashBoardCommand
         public void InitPieChart(DashBoard dab)
         {
             dabVM.LabelPoint = chartPoint => string.Format("{0:N0}", chartPoint.Y);
-            ChartValues<double> bedEmptyCount = new ChartValues<double>();
-            bedEmptyCount.Add(DataProvider.Ins.DB.BACSIs.Count());
-            ChartValues<double> bedFullCount = new ChartValues<double>();
-            bedFullCount.Add(DataProvider.Ins.DB.USERs.Count());
+
+            ChartValues<double> bacSiCount = new ChartValues<double>();
+            var bacSiList = DataProvider.Ins.DB.BACSIs.Count();
+            bacSiCount.Add(bacSiList);
+
+            ChartValues<double> yTaCount = new ChartValues<double>();
+            var yTaList = DataProvider.Ins.DB.YTAs.Count();
+            yTaCount.Add(yTaList);
+
+            ChartValues<double> benhNhanCount = new ChartValues<double>();
+            var benhNhanList = DataProvider.Ins.DB.BENHNHANs.Count();
+            benhNhanCount.Add(benhNhanList);
+
             dabVM.PieSeriesCollection = new SeriesCollection
                 {
                     new PieSeries
                     {
                         Title = "Bác sĩ",
-                        //Values = ReportDAL.Instance.QueryRevenueFromSellingInDay(currentDay, currentMonth, currentYear),
-                        Values = bedEmptyCount,
+                        Values = bacSiCount,
                         Fill = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("#FF1976D2"),
                         DataLabels = true,
                         FontSize = 16,
@@ -61,8 +70,19 @@ namespace HospitalManagement.Command.DashBoardCommand
                     new PieSeries
                     {
                         Title="Y tá",
-                        Values = bedFullCount,
+                        Values = yTaCount,
                         Fill = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("#FF27AE60"),
+                        DataLabels = true,
+                        FontSize = 16,
+                        LabelPoint = dabVM.LabelPoint,
+                    },
+
+                    new PieSeries
+                    {
+                        Title = "Bệnh nhân",
+                        //Values = ReportDAL.Instance.QueryRevenueFromSellingInDay(currentDay, currentMonth, currentYear),
+                        Values = benhNhanCount,
+                        Fill = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("pink"),
                         DataLabels = true,
                         FontSize = 16,
                         LabelPoint = dabVM.LabelPoint,
