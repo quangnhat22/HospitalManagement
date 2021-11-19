4941e24137ff0f843a601be867e9b9a1eefd98ad
Phu Quang <92578042+phuquang14722@users.noreply.github.com>
2021-11-19 23:11:31 +0700

    Add button change building

---
+++ HospitalManagement/ChangeBuildingCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using HospitalManagement.ViewModel;
+
+namespace HospitalManagement
+{
+    class ChangeBuildingCommand : ICommand
+    {
+        private RoomUsercontrolViewModel roomUsercontrolViewModel;
+
+        public ChangeBuildingCommand(RoomUsercontrolViewModel roomUsercontrolViewModel)
+        {
+            this.roomUsercontrolViewModel = roomUsercontrolViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Button btn = parameter as Button;
+            int currentBuilding = roomUsercontrolViewModel.CurrentBuilding;
+            
+            if (btn.Name == "btnLeft")
+            {
+                if (currentBuilding > 1)
+                    currentBuilding--;
+            }
+            else
+            {
+                if (currentBuilding < roomUsercontrolViewModel.Buildings.Count)
+                    currentBuilding++;
+            }
+            roomUsercontrolViewModel.CurrentBuilding = currentBuilding;
+        }
+    }
+}
--- HospitalManagement/ViewModel/RoomUsercontrolViewModel.cs
+++ HospitalManagement/ViewModel/RoomUsercontrolViewModel.cs
@@ -6,17 +6,39 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
 using HospitalManagement.Command;
+using HospitalManagement.Model;
 
 namespace HospitalManagement.ViewModel
 {
     class RoomUsercontrolViewModel : BaseViewModel
     {
         public ICommand OpenRoomWindow { get; set; }
+        public ICommand ChangeBuildingCommand { get; set; }
+        public ICommand DisableButtonCommand { get; set; }
+        
         public string Tag { get; }
+
+        private int currentBuilding;
+        public int CurrentBuilding
+        {
+            get { return currentBuilding; }
+            set { currentBuilding = value; OnPropertyChanged("CurrentBuilding"); }
+        }
+
+        private static List<TOA> buildings;
+        public List<TOA> Buildings
+        {
+            get { return buildings; }
+            set { buildings = value; OnPropertyChanged("Buildings"); }
+        }
 
         public RoomUsercontrolViewModel()
         {
+            currentBuilding = 1;
+            Buildings = DataProvider.Ins.DB.TOAs.ToList();
             OpenRoomWindow = new OpenRoomWindowCommand();
+            ChangeBuildingCommand = new ChangeBuildingCommand(this);
+            DisableButtonCommand = new DisableButtonCommand();
         }
     }
 }
