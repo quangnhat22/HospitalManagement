2b44b82282a83ed3b165ead18454ba43659e8d73
Bùi Minh Tuấn <92067348+tuan20520342@users.noreply.github.com>
2021-11-21 15:08:00 +0700

    Add doctor

---
+++ HospitalManagement/Command/DoctorListCommand/AddDoctorCommand.cs
@@ -0,0 +1,57 @@
+﻿using HospitalManagement.Model;
+using HospitalManagement.View;
+using HospitalManagement.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HospitalManagement.Command
+{
+    class AddDoctorCommand : ICommand
+    {
+        private DoctorFormViewModel doctorFormViewModel;
+
+        public AddDoctorCommand(DoctorFormViewModel doctorFormViewModel)
+        {
+            this.doctorFormViewModel = doctorFormViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return parameter == null ? false : true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DoctorForm doctorForm = parameter as DoctorForm;
+            string gioiTinh = doctorForm.cbxGioiTinh.SelectedValue as string;
+            var doctorInput = new BACSI
+            {
+                HO = doctorForm.txtHo.Text,
+                TEN = doctorForm.txtTen.Text,
+                SDT = doctorForm.txtSDT.Text,
+                EMAIL = doctorForm.txtEmail.Text,
+                DIACHI = doctorForm.txtDiaChi.Text,
+                NGSINH = doctorForm.txtNGSinh.DisplayDate,
+                GIOITINH = (gioiTinh == "Nam"),
+                QUOCTICH = doctorForm.txtQuocTich.Text,
+                CMND_CCCD = doctorForm.txtCMND_CCCD.Text,
+                CHUYENKHOA = doctorForm.txtChuyenKhoa.Text,
+                VAITRO = doctorForm.txtVaiTro.Text,
+                IDTO = Convert.ToInt32(doctorForm.txtIDTO.Text),
+                GHICHU = doctorForm.txtGhiChu.Text,
+            };
+            doctorFormViewModel?.db?.DB?.BACSIs.Add(doctorInput);
+            doctorFormViewModel?.db?.DB?.SaveChanges();
+        }
+    }
+}
---
+++ HospitalManagement/ViewModel/DoctorFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using HospitalManagement.Command;
+using HospitalManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HospitalManagement.ViewModel
+{
+    internal class DoctorFormViewModel : BaseViewModel
+    {
+        public DataProvider db;
+        public ICommand AddDoctor { get; set; }
+        public DoctorFormViewModel()
+        {
+            db = new DataProvider();
+            AddDoctor = new AddDoctorCommand(this);
+        }
+    }
+}
