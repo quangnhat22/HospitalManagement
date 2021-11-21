6f2687a6284ae1a6280a978fa561fe99428c5c89
quangSotech <20520719@gm.uit.edu.vn>
2021-11-22 01:48:57 +0700

    update LINQ column chart

--- HospitalManagement/Utils/ToaTK.cs
+++ HospitalManagement/Utils/ToaTK.cs
@@ -5,12 +5,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HospitalManagement.Utils
 {
     public class ToaTK
     {
-        public static ChartValues<int> NangList ;
+        public static ChartValues<int> NangList;
         public static ChartValues<int> TrungBinhList;
         public static ChartValues<int> NheList;
         public ToaTK()
@@ -22,34 +23,40 @@ namespace HospitalManagement.Utils
 
         public void thongKeBenhNhanTheoToa()
         {
-            List<TOA> listToa = DataProvider.Ins.DB.TOAs.ToList();
-            foreach (TOA toa in listToa)
+            var result = from toa in DataProvider.Ins.DB.TOAs
+                         join tang in DataProvider.Ins.DB.TANGs on toa.IDTOA  equals tang.IDTOA into table1
+                         from tang in table1.ToList()
+                         join phong in DataProvider.Ins.DB.PHONGs on tang.ID equals phong.IDTANG  into table2
+                         from phong in table2.ToList()
+                         join benhNhan in DataProvider.Ins.DB.BENHNHANs on phong.ID equals benhNhan.IDPHONG into table3
+                         from benhNhan in table3.ToList()
+                         group benhNhan by toa.IDTOA into toaGroup
+                         select new
+                         {
+                             idToa = toaGroup.Key,
+                             toaItem = toaGroup.ToList(),
+                         };
+            result.ToList().ForEach(to =>
             {
                 int soLuongBN_Nang = 0;
                 int soLuongBN_TB = 0;
                 int soLuongBN_Nhe = 0;
-                List<TANG> listTang = toa.TANGs.ToList();
-                foreach (TANG tang in listTang)
+                to.toaItem.ForEach(x =>
                 {
-                    List<PHONG> listPhong = tang.PHONGs.ToList();
-                    foreach (PHONG phong in listPhong)
-                    {
-                        List<BENHNHAN> listBN = phong.BENHNHANs.ToList();
-                        foreach (BENHNHAN bn in listBN)
-                        {
-                            if (bn.TINHTRANG == "Triệu chứng trở nặng")
-                                soLuongBN_Nang++;
-                            else if (bn.TINHTRANG == "Có triệu chứng")
-                                soLuongBN_TB++;
-                            else
-                                soLuongBN_Nhe++;
-                        }
-                    }
-                }
+                    if (x.TINHTRANG == "Triệu chứng trở nặng")
+                        soLuongBN_Nang++;
+                    else if (x.TINHTRANG == "Có triệu chứng")
+                        soLuongBN_TB++;
+                    else
+                        soLuongBN_Nhe++;
+                });
                 NangList.Add(soLuongBN_Nang);
                 TrungBinhList.Add(soLuongBN_TB);
                 NheList.Add(soLuongBN_Nhe);
-            }
+                soLuongBN_Nang = 0;
+                soLuongBN_TB = 0;
+                soLuongBN_Nhe = 0;
+            });
         }
     }
 }
--- HospitalManagement/ViewModel/DashBoardViewModel.cs
+++ HospitalManagement/ViewModel/DashBoardViewModel.cs
@@ -51,41 +51,6 @@ namespace HospitalManagement.ViewModel
             BedCount = DataProvider.Ins.DB.PHONGs.Count() * 6;
 
             #region "Initial Stacked Column Chart"
-
-            //danh sach F0 o cac toa
-            
-            //List<TOA> listToa = DataProvider.Ins.DB.TOAs.ToList();
-            //ChartValues<int> NangList = new ChartValues<int>();
-            //ChartValues<int> TrungBinhList = new ChartValues<int>();
-            //ChartValues<int> NheList = new ChartValues<int>();
-
-            //foreach (TOA toa in listToa)
-            //{
-            //    int soLuongBN_Nang = 0;
-            //    int soLuongBN_TB = 0;
-            //    int soLuongBN_Nhe = 0;
-            //    List<TANG> listTang = toa.TANGs.ToList();
-            //    foreach(TANG tang in listTang)
-            //    {
-            //        List<PHONG> listPhong = tang.PHONGs.ToList();
-            //        foreach(PHONG phong in listPhong)
-            //        {
-            //            List<BENHNHAN> listBN = phong.BENHNHANs.ToList();   
-            //            foreach(BENHNHAN bn in listBN)
-            //            {
-            //                if(bn.TINHTRANG == "Triệu chứng trở nặng")
-            //                    soLuongBN_Nang++;
-            //                else if(bn.TINHTRANG =="Có triệu chứng")
-            //                    soLuongBN_TB++;
-            //                else 
-            //                    soLuongBN_Nhe++;
-            //            }
-            //        }
-            //    }
-            //    NangList.Add(soLuongBN_Nang);
-            //    TrungBinhList.Add(soLuongBN_TB);
-            //    NheList.Add(soLuongBN_Nhe);
-            //}
             ToaTK toaTK = new ToaTK();
             toaTK.thongKeBenhNhanTheoToa();
            
