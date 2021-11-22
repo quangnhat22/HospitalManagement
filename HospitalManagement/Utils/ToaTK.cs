b539f3d6450d51b3dc4a3105cf00f2b97b2fe8e3
quangSotech <20520719@gm.uit.edu.vn>
2021-11-22 10:59:28 +0700

    fix linq

--- HospitalManagement/Utils/ToaTK.cs
+++ HospitalManagement/Utils/ToaTK.cs
@@ -21,27 +21,50 @@ namespace HospitalManagement.Utils
             NheList = new ChartValues<int>();
         }
 
-        public void thongKeBenhNhanTheoToa()
+        public async void thongKeBenhNhanTheoToa()
         {
-            var result = from toa in DataProvider.Ins.DB.TOAs
-                         join tang in DataProvider.Ins.DB.TANGs on toa.IDTOA  equals tang.IDTOA into table1
-                         from tang in table1.ToList()
-                         join phong in DataProvider.Ins.DB.PHONGs on tang.ID equals phong.IDTANG  into table2
-                         from phong in table2.ToList()
-                         join benhNhan in DataProvider.Ins.DB.BENHNHANs on phong.ID equals benhNhan.IDPHONG into table3
-                         from benhNhan in table3.ToList()
-                         group benhNhan by toa.IDTOA into toaGroup
-                         select new
-                         {
-                             idToa = toaGroup.Key,
-                             toaItem = toaGroup.ToList(),
-                         };
+            //var result = from toa in DataProvider.Ins.DB.TOAs
+            //             join tang in DataProvider.Ins.DB.TANGs on toa.IDTOA  equals tang.IDTOA into table1
+            //             from tang in table1.ToList()
+            //             join phong in DataProvider.Ins.DB.PHONGs on tang.ID equals phong.IDTANG into table2
+            //             from phong in table2.ToList()
+            //             join benhNhan in DataProvider.Ins.DB.BENHNHANs on phong.ID equals benhNhan.IDPHONG into table3
+            //             from benhNhan in table3.ToList()
+            //             group benhNhan by toa.IDTOA into toaGroup
+            //             select new
+            //             {
+            //                 idToa = toaGroup.Key,
+            //                 toaItem = toaGroup.ToList(),
+            //             };
+            var connectionToaTang = from toa in DataProvider.Ins.DB.TOAs
+                                    join tang in DataProvider.Ins.DB.TANGs on toa.IDTOA equals tang.IDTOA
+                                    select new
+                                    {
+                                        idToa = toa.IDTOA,
+                                        idTang = tang.ID
+                                    };
+            var connectionTangPhong = from tang in connectionToaTang
+                                      join phong in DataProvider.Ins.DB.PHONGs on tang.idTang equals phong.IDTANG
+                                      select new
+                                      {
+                                          idToa = tang.idToa,
+                                          idPhong = phong.ID
+                                      };
+            var result = from phong in connectionTangPhong
+                                          join benhNhan in DataProvider.Ins.DB.BENHNHANs on phong.idPhong equals benhNhan.IDPHONG
+                                          group benhNhan by phong.idToa into listBenhNhan
+                                          select new
+                                          {
+                                              idToa = listBenhNhan.Key,
+                                              benhNhans = listBenhNhan.ToList()
+                                          };
+
             result.ToList().ForEach(to =>
             {
                 int soLuongBN_Nang = 0;
                 int soLuongBN_TB = 0;
                 int soLuongBN_Nhe = 0;
-                to.toaItem.ForEach(x =>
+                to.benhNhans.ForEach(x =>
                 {
                     if (x.TINHTRANG == "Triệu chứng trở nặng")
                         soLuongBN_Nang++;
