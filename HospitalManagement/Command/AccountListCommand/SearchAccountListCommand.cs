4f917163986c487096aa9c1acc565d605ef7f596
quangSotech <20520719@gm.uit.edu.vn>
2021-11-25 17:08:40 +0700

    add filter account

--- HospitalManagement/Command/AccountListCommand/SearchAccountListCommand.cs
+++ HospitalManagement/Command/AccountListCommand/SearchAccountListCommand.cs
@@ -1,12 +1,98 @@
-﻿using System;
+﻿using HospitalManagement.Model;
+using HospitalManagement.Utils;
+using HospitalManagement.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HospitalManagement.Command.AccountListCommand
 {
-    internal class SearchAccountListCommand
+    public class SearchAccountListCommand : ICommand
     {
+        private AccountListViewModel accountListViewModel;
+
+        public SearchAccountListCommand(AccountListViewModel accountListViewModel)
+        {
+            this.accountListViewModel = accountListViewModel;   
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //cmnd,ten,email,tendangnhap,tocongtac
+            accountListViewModel.SearchBox = accountListViewModel.SearchBox.Trim();
+            if (accountListViewModel.SearchBox == string.Empty || accountListViewModel.SearchBox == null)
+            {
+                accountListViewModel.Users = SelectableItem<StaffInformation>.GetSelectableItems(accountListViewModel.StaffAccounts);
+            }
+            else
+            {
+                string RemoveSignString = VietnameseSign.convertToUnSign2(accountListViewModel.SelectedFilter).ToLower();
+                if (RemoveSignString == "cmnd/cccd")
+                {
+                    accountListViewModel.Users = SelectableItem<StaffInformation>.GetSelectableItems(
+                                                                                            accountListViewModel.StaffAccounts.Where(
+                                                                                                p => p.Cmnd_cccd.Contains(accountListViewModel.SearchBox)).ToList());
+                }
+                else if (RemoveSignString == "ho")
+                {
+                    accountListViewModel.Users = SelectableItem<StaffInformation>.GetSelectableItems(
+                        accountListViewModel.StaffAccounts.Where(delegate (StaffInformation bn)
+                        {
+                            return VietnameseSign.ContainsUnsigned(bn.Ho, accountListViewModel.SearchBox);
+                        }).AsQueryable().ToList()
+                        );
+                }
+                else if (RemoveSignString == "ten")
+                {
+                    accountListViewModel.Users = SelectableItem<StaffInformation>.GetSelectableItems(
+                        accountListViewModel.StaffAccounts.Where(delegate (StaffInformation bn)
+                        {
+                            return VietnameseSign.ContainsUnsigned(bn.Ten, accountListViewModel.SearchBox);
+                        }).AsQueryable().ToList()
+                        );
+                }
+                else if (RemoveSignString == "email")
+                {
+                    accountListViewModel.Users = SelectableItem<StaffInformation>.GetSelectableItems(
+                        accountListViewModel.StaffAccounts.Where(delegate (StaffInformation bn)
+                        {
+                            return VietnameseSign.ContainsUnsigned(bn.Email, accountListViewModel.SearchBox);
+                        }).AsQueryable().ToList()
+                        );
+                }
+                else if (RemoveSignString == "ten dang nhap")
+                {
+                    accountListViewModel.Users = SelectableItem<StaffInformation>.GetSelectableItems(
+                        accountListViewModel.StaffAccounts.Where(delegate (StaffInformation bn)
+                        {
+                            return VietnameseSign.ContainsUnsigned(bn.UserName , accountListViewModel.SearchBox);
+                        }).AsQueryable().ToList()
+                        );
+                }
+                else if (RemoveSignString == "to")
+                {
+                    accountListViewModel.Users = SelectableItem<StaffInformation>.GetSelectableItems(
+                        accountListViewModel.StaffAccounts.Where(delegate (StaffInformation bn)
+                        {
+                            return VietnameseSign.ContainsUnsigned(bn.IdTo.ToString() , accountListViewModel.SearchBox);
+                        }).AsQueryable().ToList()
+                        );
+                }
+            }
+        }
     }
 }
