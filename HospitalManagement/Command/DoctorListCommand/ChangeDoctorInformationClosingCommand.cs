fc2ca23538219ff58bc5d9b8da50a24a0b3080a3
Bùi Minh Tuấn <92067348+tuan20520342@users.noreply.github.com>
2021-11-27 16:17:54 +0700

    Fix closing

--- HospitalManagement/Command/DoctorListCommand/ChangeDoctorInformationClosingCommand.cs
+++ HospitalManagement/Command/DoctorListCommand/ChangeDoctorInformationClosingCommand.cs
@@ -1,25 +1,20 @@
 ﻿using HospitalManagement.Model;
 using HospitalManagement.Utils;
 using HospitalManagement.View;
+using HospitalManagement.View.Staff;
 using HospitalManagement.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace HospitalManagement.Command
 {
     internal class ChangeDoctorInformationClosingCommand : ICommand
     {
-        private DoctorInformationViewModel doctorFormViewModel;
-        public ChangeDoctorInformationClosingCommand(DoctorInformationViewModel doctorFormViewModel)
-        {
-            this.doctorFormViewModel = doctorFormViewModel;
-        }
-
-
         public event EventHandler CanExecuteChanged
         {
             add { }
@@ -28,14 +23,30 @@ namespace HospitalManagement.Command
 
         public bool CanExecute(object parameter)
         {
-            return parameter == null ? false : true;
+            return true;
         }
-
         public void Execute(object parameter)
         {
-            DoctorForm doctorForm = parameter as DoctorForm;
-            NotifyWindow notifyWindow = new NotifyWindow("Success", "OK");
-            notifyWindow.ShowDialog();
+            ChangeDoctorInformationForm doctorForm = parameter as ChangeDoctorInformationForm;
+            BACSI bs = DataProvider.Ins.DB.BACSIs.Find(doctorForm.txbCMND_CCCD.Text);
+            DataProvider.Ins.DB.Entry<BACSI>(bs).Reload();
+            doctorForm.txbHo.Text = bs.HO;
+            doctorForm.txbTen.Text = bs.TEN;
+            doctorForm.txbChuyenKhoa.Text = bs.CHUYENKHOA;
+            doctorForm.txbQuocTich.Text = bs.QUOCTICH;
+            doctorForm.txbDiaChi.Text = bs.DIACHI;
+            doctorForm.txbEmail.Text = bs.EMAIL;
+            doctorForm.txbGhiChu.Text = bs.GHICHU;
+            doctorForm.txbSDT.Text = bs.SDT;
+            doctorForm.txbVaiTro.Text = bs.VAITRO;
+            doctorForm.txbIDTO.Text = bs.IDTO.ToString();
+            if ((bool)bs.GIOITINH)
+            {
+                doctorForm.cbxGioiTinh.Text = "Nữ";
+            }
+            else doctorForm.cbxGioiTinh.Text = "Nam";
+            DateTime text = (DateTime)bs.NGSINH;
+            string date = text.ToString("dd/MM/yyyy");          
         }
     }
 }
