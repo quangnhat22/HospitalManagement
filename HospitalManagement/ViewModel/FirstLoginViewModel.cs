da68bfa2ac346602c8c4cb995af535180d1391b6
quangSotech <20520719@gm.uit.edu.vn>
2021-12-13 07:28:51 +0700

    update first login window

---
+++ HospitalManagement/ViewModel/FirstLoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using HospitalManagement.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HospitalManagement.ViewModel
+{
+    internal class FirstLoginViewModel : BaseViewModel
+    {
+        public ICommand OpenChangeAccountWindowCommand { get; set; }
+
+        public FirstLoginViewModel()
+        {
+            OpenChangeAccountWindowCommand = new OpenChangeAccoutWindowCommand();
+        }
+    }
+}
