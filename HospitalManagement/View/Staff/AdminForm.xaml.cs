27d4ffc60c9484d8180976268d3940218e3cde0e
Bùi Minh Tuấn <92067348+tuan20520342@users.noreply.github.com>
2021-12-17 19:49:45 +0700

    Update UI

--- HospitalManagement/View/Staff/AdminForm.xaml.cs
+++ HospitalManagement/View/Staff/AdminForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalManagement.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,10 @@ namespace HospitalManagement.View.Staff
     /// </summary>
     public partial class AdminForm : Window
     {
-        public AdminForm()
+        public AdminForm(ADMIN ad)
         {
             InitializeComponent();
+
         }
     }
 }
